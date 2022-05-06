8830a5549e662bed5e4c110925271db6c79e33a0
Barış Bayazıt <bayazitbaris@gmail.com>
2022-05-06 23:20:00 +0300

    Better eslint config + eslint fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import type { NextPage } from "next";
+import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex h-screen bg-slate-200">
-      <div className="m-auto justify-center items-center ">
-        <div className="bg-slate-400 rounded-xl">
-          <p className="p-2 pr-6 pl-6">
+    return (
+        <div className="flex h-screen bg-slate-200">
+            <div className="m-auto justify-center items-center ">
+                <div className="bg-slate-400 rounded-xl">
+                    <p className="p-2 pr-6 pl-6">
             One day, this text will go into the history books. Screenshot this!
-          </p>
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Home;
+export default Home
