b5fb05decb57f69fdd8ee9f74ff696b4146b8feb
Barış Bayazıt <bayazitbaris@gmail.com>
2022-06-12 17:50:55 +0300

    Enter token addresses progess

--- pages/components/CreateDao/step-two.tsx
+++ pages/components/CreateDao/step-two.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 import TypewriterComponent from "typewriter-effect"
 
@@ -16,9 +16,13 @@ interface StepTwoProps {
 const StepTwo = ({ closeHandler, setDaoName, setDaoLogo, setTokenMetadata }: StepTwoProps) => {
     const [titleTyped, setTitleTyped] = useState<boolean>(false)
     const [showInputs, setShowInputs] = useState<boolean>(false)
+    const [daoNameError, setDaoNameError] = useState<string>("")
+    const [inputError, setInputError] = useState<string>("")
+
+    const daoNameRef = useRef() as React.MutableRefObject<HTMLInputElement>
 
     return (
-        <div className="h-full w-full space-y-4">
+        <div className="flex flex-col h-full w-full space-y-3">
             <h1 className="text-3xl font-semibold">
                 {titleTyped ? (
                     "Welcome to Democrazy!"
@@ -47,15 +51,42 @@ const StepTwo = ({ closeHandler, setDaoName, setDaoLogo, setTokenMetadata }: Ste
                 />
             </p>
             {showInputs ? (
-                <div className="flex flex-col space-y-4">
+                <div className="flex flex-col space-y-6 h-full w-full">
                     <div className="flex space-x-4">
-                        <input
-                            type="text"
-                            className="rounded-lg bg-gray-900 border-2 border-black w-1/2 p-1 text-sm"
-                            maxLength={32}
-                        />
-                        <div className="justify-center m-auto">
-                            <p className="text-red-200 text-sm">{2}</p>
+                        <div className="flex w-full flex-col">
+                            <label>DAO Name</label>
+                            <input
+                                type="text"
+                                placeholder="DAO Name"
+                                className="rounded-lg bg-gray-900 border-2 border-black w-3/4 p-1 text-sm"
+                                maxLength={32}
+                                ref={daoNameRef}
+                            />
+                            <p className="text-red-200 text-sm">{daoNameError}</p>
+                        </div>
+                        <div className="flex w-full flex-col">
+                            <label>Upload DAO Logo</label>
+                            <input
+                                type="file"
+                                className="block rounded bg-clip-padding focus:outline-none border-gray-400 bg-gray-700 border-[1px]"
+                                accept="image/*"
+                            />
+                            <p className="text-red-200 text-sm">{inputError}</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col h-full">
+                        <div className="flex scale-90 origin-left">
+                            <div className="flex flex-row space-x-4 w-11/12">
+                                <label className="flex items-center">Ethereum:</label>
+                                <input
+                                    type="text"
+                                    placeholder="Token address (empty if none)"
+                                    className="rounded-lg bg-gray-900 border-2 border-black w-1/2 p-1 text-sm"
+                                    maxLength={42}
+                                    ref={daoNameRef}
+                                />
+                                <p className="flex items-center text-red-200 text-sm">{daoNameError}</p>
+                            </div>
                         </div>
                     </div>
                     <div className="flex space-x-8">
